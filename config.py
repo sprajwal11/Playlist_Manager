031471a85c090e50c8aee65286477977b8580f0f
Prajwal <sharmaprajwal11@gmail.com>
2023-08-23 06:50:11 +0530

    removed unnecessary files

--- config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    SECRET_KEY = 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:123456@localhost/music_library'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
